d299ab46aadde768d4d69b4130c570e6106c2ff6
Nick Wright <nwright970@gmail.com>
2023-07-30 18:52:30 +0000

    Update descriptions to be slightly more meaningful

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2023 Nick Wright nwright970@gmail.com
-
 */
 package cmd
 
@@ -17,13 +16,8 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "barnacle-net",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Control a network of barnacle nodes.",
+	Long:  `Control a network of barnacle nodes.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
--- cmd/server.go
+++ cmd/server.go
@@ -30,13 +30,8 @@ import (
 // serverCmd represents the server command
 var serverCmd = &cobra.Command{
 	Use:   "server",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Control for a barnacle-net server.",
+	Long:  `Control for a barnacle-net server.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("server called")
 	},
